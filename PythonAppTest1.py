6cfdb2d0ff89754a6fda881d30c260eee859fd5f
日 赵 <ri1982@hotmail.com>
2020-05-31 17:53:17 +0800

    test3

--- PythonAppTest1.py
+++ PythonAppTest1.py
@@ -1,1 +1,9 @@
-print('hello Vistual studio for Python~')
+def method_name():
+    print('hello Vistual studio for Python~')
+    print('test end')
+
+method_name()
+#netshare
+#let us program it together
+# it is great, wonderful
+#hello it is ok, bye
